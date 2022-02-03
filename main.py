4ece516bcc923e22ed8c88ff04c1ba7362ec72cf
ZhenyaRybakov <fishrybakov@gmail.com>
2022-02-03 19:59:55 +0300

    Прописана логика для хода компьютера

--- main.py
+++ main.py
@@ -1,45 +1,65 @@
 
 
+
 def make_table():
+    pass
+
+def update_table():
+    pass
+
+
+def human_step(game_table):
+    while True:
+        step_number = input('Введи номер элемента - ', )
+        try:
+            if int(step_number) < 10:
+                game_table[0][int(a)] = 'X'
+            elif int(step_number) > 99:
+                print('Введите число от 0 до 99')
+            else:
+                step_number = tuple(step_number)
+                print(step_number)
+                game_table[int(step_number[0])][int(step_number[1])] = 'X'
+        except ValueError:
+            print('А где же число?')
+            continue
+        print(*game_table, sep='\n')
+        computer_step(game_table)
+
+
+def computer_step(game_table):
+    print('Теперь ходит компьютер')
+    import random
+    step = random.randint(0, 100)
+    print('Компьютер выбрал номер - ', step)
+    if step < 10:
+        if game_table[0][step] == 'X':
+            computer_step(game_table)
+        else:
+            game_table[0][step] = 'O'
+    else:
+        coordinat_step = [int(x) for x in str(step)]
+
+        if game_table[coordinat_step[0]][coordinat_step[1]] == 'X':
+            computer_step(game_table)
+        else:
+            game_table[coordinat_step[0]][coordinat_step[1]] = 'O'
+    print(*game_table, sep='\n')
+    human_step(game_table)
+
+
+def start_game():
     list_game_numbers = [str(x) for x in range(100)]
     game_table = []
     i = 0
     for j in range(10):
         game_table.append(list_game_numbers[i:i + 10])
         i += 10
-    return game_table
-
-def update_table():
-    pass
-
-def human_step(make_table):
-    while True:
-        a = input('Введи номер элемента - ', )
-        print(a)
-        try:
-            if int(a) < 10:
-                game_table[0][int(a)] = 'X'
-            elif int(a) > 99:
-                print('Введите число от 0 до 99')
-            else:
-                a = tuple(a)
-                print(a)
-                game_table[int(a[0])][int(a[1])] = 'X'
-        except ValueError:
-            print('А где же число?')
-            continue
+    print(*game_table, sep='\n')
+    human_step(game_table)
 
 
-def computer_step(update_table):
-    import random
-    step = random.randint(0, 100)
-    if step<10:
-        update_table[0][step] = 'O'
-    else:
-        coordinat_step = [int(x) for x in step]
-        update_table[coordinat_step[0]][coordinat_step[1]] = 'O'
-
-    return update_table
+start_game()
 
 
 # list_game_numbers = [str(x) for x in range(100)]
@@ -75,5 +95,5 @@ def computer_step(update_table):
 #     print(*game_table, sep='\n')
 
 
-if __name__ == "__main__":
-	print(*make_table(), sep='\n')
+# if __name__ == "__main__":
+#
