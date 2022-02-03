507b508ba236a330d039eb828381aef316c2d79f
ZhenyaRybakov <fishrybakov@gmail.com>
2022-02-03 20:47:22 +0300

    Реализована функция valid_step

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 
+def loss_check(game_table, step):
+    print('Проверил')
 
 
-def make_table():
-    pass
 
-def update_table():
-    pass
 
 
 def human_step(game_table):
-    while True:
-        step_number = input('Введи номер элемента - ', )
-        try:
-            if int(step_number) < 10:
-                game_table[0][int(step_number)] = 'X'
-            elif int(step_number) > 99:
-                print('Введите число от 0 до 99')
-            else:
-                step_number = tuple(step_number)
-                print(step_number)
-                game_table[int(step_number[0])][int(step_number[1])] = 'X'
-        except ValueError:
-            print('А где же число?')
-            continue
-        print(*game_table, sep='\n')
-        return game_table
-        # computer_step(game_table)
+    try:
+        step = int(input('Введи номер элемента - ', ))
+    except ValueError:
+        print('Введите число от 0 до 99')
+        human_step(game_table)
+    valid_step(step, game_table, gamer='human')
+    print(*game_table, sep='\n')
+    print('_' * 75)
+    return game_table
 
 
 def computer_step(game_table):
@@ -33,19 +23,39 @@ def computer_step(game_table):
     import random
     step = random.randint(0, 100)
     print('Компьютер выбрал номер - ', step)
+    valid_step(step, game_table, gamer='computer')
+    print(*game_table, sep='\n')
+    print('_'*75)
+    return game_table
+
+
+def valid_step(step, game_table, gamer):
     if step < 10:
-        if game_table[0][step] == 'X':
-            computer_step(game_table)
+        if game_table[0][step] in ('X', 'O'):
+            print('Этот номер уже занят, попробуй другой')
+            if gamer == 'computer':
+                computer_step(game_table)
+            else:
+                human_step(game_table)
         else:
-            game_table[0][step] = 'O'
+            if gamer == 'computer':
+                game_table[0][step] = 'O'
+            else:
+                game_table[0][step] = 'X'
     else:
         coordinat_step = [int(x) for x in str(step)]
-
-        if game_table[coordinat_step[0]][coordinat_step[1]] == 'X':
-            computer_step(game_table)
+        if game_table[coordinat_step[0]][coordinat_step[1]] in ('X', 'O'):
+            print('Этот номер уже занят, попробуй другой')
+            if gamer == 'computer':
+                computer_step(game_table)
+            else:
+                human_step(game_table)
         else:
-            game_table[coordinat_step[0]][coordinat_step[1]] = 'O'
-    print(*game_table, sep='\n')
+            if gamer == 'computer':
+                game_table[coordinat_step[0]][coordinat_step[1]] = 'O'
+            else:
+                game_table[coordinat_step[0]][coordinat_step[1]] = 'X'
+    loss_check(game_table, step)
     return game_table
 
 
@@ -59,42 +69,8 @@ def start_game():
     print(*game_table, sep='\n')
     while True:
         game_table = human_step(game_table)
+
         game_table = computer_step(game_table)
+
+
 start_game()
-
-
-# list_game_numbers = [str(x) for x in range(100)]
-# game_table = []
-# i = 0
-# for j in range(10):
-#     game_table.append(list_game_numbers[i:i+10])
-#     i += 10
-#
-# print(*game_table, sep='\n')
-#
-# while True:
-#
-#     '''Разбить чило на элементы, использоваить int(input()) '''
-#
-#
-#     a = input('Введи номер элемента - ', )
-#
-#     print (a)
-#     try:
-#         if int(a)<10:
-#             game_table[0][int(a)] ='X'
-#         elif int(a)>99:
-#             print('Введите число от 0 до 99')
-#         else:
-#             a = tuple(a)
-#             print(a)
-#             game_table[int(a[0])][int(a[1])] = 'X'
-#     except ValueError:
-#         print('А где же число?')
-#         continue
-#
-#     print(*game_table, sep='\n')
-
-
-# if __name__ == "__main__":
-#
